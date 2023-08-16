9139c3ab6aab7357a18463066bac246c66f4a1d7
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-08-16 13:11:27 -0300

    fix tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -18,6 +18,6 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/api/v1').expect(200).expect({ version: '0.0.1', env: 'test' });
+    return request(app.getHttpServer()).get('/api/v1').expect(200).expect({ version: '0.0.1', env: 'qa' });
   });
 });
