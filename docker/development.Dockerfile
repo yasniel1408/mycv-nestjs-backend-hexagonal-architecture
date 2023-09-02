5884f3768d06f0248203175ebd03f526ad5a8de1
Yasniel <yasnielfajardoegues1408@gmail.com>
2023-09-02 03:37:09 -0300

    refactor

--- docker/development.Dockerfile
+++ docker/development.Dockerfile
@@ -14,6 +14,4 @@ RUN chmod a+rx /usr/src/node_modules/bcrypt/lib/binding/napi-v3/bcrypt_lib.node
 
 RUN npm rebuild bcrypt --update-binary
 
-RUN npm run typeorm migration:run -- -d src/config/db/migrations
-
 CMD ["npm", "run", "start:dev"]
--- src/config/db/database.config.ts
+++ src/config/db/database.config.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import { isLocal, isQa } from '../constants';
+import { isLocal, isTest } from '../constants';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
@@ -14,9 +14,9 @@ const typeOrmConfig = (configService: ConfigService) =>
     username: configService.getOrThrow<string>('DB_USERNAME'),
     password: configService.getOrThrow<string>('DB_PASSWORD'),
     database: configService.getOrThrow<string>('DB_DATABASE'),
-    entities: [join(__dirname, '../../../**/**.dao{.ts,.js}')],
-    migrations: [join(__dirname, '../**/**.migration{.ts,.js}')],
-    synchronize: isLocal || isQa ? true : false, // esto solo es para desarrollo, para produccion usamos migraciones
+    entities: [join(__dirname, 'src/**/**.dao{.ts,.js}')],
+    migrations: [join(__dirname, 'src/**/**.migration{.ts,.js}')],
+    synchronize: isLocal || isTest ? true : false, // esto solo es para desarrollo, para produccion usamos migraciones
     logging: false, // esto es para debugear las consultas a la base de datos
   } as TypeOrmModuleOptions);
 
